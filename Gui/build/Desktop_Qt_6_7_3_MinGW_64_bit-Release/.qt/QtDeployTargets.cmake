a7da6071803878cdcf26e2b794b1284131b72635
Ibrahim Mohamed <hes93314@gmail.com>
2024-11-02 20:02:45 +0200

    add new version

--- Gui/build/Desktop_Qt_6_7_3_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appGui_FILE D:/Gui/build/Desktop_Qt_6_7_3_MinGW_64_bit-Release/appGui.exe)
-set(__QT_DEPLOY_TARGET_appGui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appGui_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
