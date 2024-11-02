0f7a1d24a2d4bb4daa60fea3d86a8256512396e8
Ibrahim Mohamed <hes93314@gmail.com>
2024-11-02 20:23:25 +0200

    add final version

--- A1-Task4Task5-S1-20230003_20230016_20230233.cpp
+++ A1-Task4Task5-S1-20230003_20230016_20230233.cpp
@@ -1,3 +1,33 @@
+// ==================================================================================== //
+// FCAI – Object-Oriented Programming – 2024 - Assignment                               //
+//                                                                                      //
+// File:    A1-Task4Task5-S1-20230003_20230016_20230233.cpp                             //
+//                                                                                      //
+// Purpose: This provides a detailed explanation of the VOLE Machine s code,            //
+//          which is structured into multiple classes, each responsible for             //
+//          different functionalities of the machine. These classes include             //
+//          Register, ALU (Arithmetic Logic Unit), Memory, Instructions, and            //
+//          Machine. Each class encapsulates specific operations and data,              //
+//          creating a modular and organized virtual machine for executing              //
+//          instructions.                                                               //
+//                                                                                      //
+// Author:  Ibrahim Mohamed Saad Mohamed_S1_20230003 => Main, Register                  //
+//          Ahmed Hussein Mohamed Ahmed_S1_20230016  => Memory, Instruction             //
+//          Ezz eldin Omar Abd El-latif_S1_20230233  => Main, ALU                       //
+//                                                                                      //
+// Emails:  hes93314@gmail.com                                                          //
+//          hzrdu3500@gmail.com                                                         //
+//          ezzeldinomar7@gmail.com                                                     //
+//                                                                                      //
+// TA name: Bassant Samer                                                               //
+// Date:    11/2/2024                                                                   //
+// Version: 2.0                                                                         //
+// ==================================================================================== //
+
+// ==================================================================================== //
+//              Gui Repository Link: https://github.com/QNQ124/Vole_Machine             //
+// ==================================================================================== //
+
 #include <iostream>
 #include "volemachine.h"
 #include <bits/stdc++.h>
